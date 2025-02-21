d267f0feeeccb9c965b950bbac36e1b4d44dc639
Johannes Medagbe <johanmedagbe@gmail.com>
2025-02-21 11:36:32 +0300

    Data added and deep learning scripts written

---
+++ scripts/common_dl.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""
+This script contains deep learning finetuning class for general (binary or multi) classification
+"""
+
+import tensorflow as tf
+import keras_tuner as kt # for finetuning
+
+
+class ClassificationSpecies(kt.HyperModel):
+    """
+    Represents hyperparameter tuning for clustering task
+    """
+    
+    def build(self, hp):
+        """
+        Finetuning at the model level
+        """
+        
+        # Specify possible values of each hyperparameter for search
+        
+        n_hidden=hp.Int('n_hidden', min_value=1, max_value=20, default=10)
+        n_neurons=hp.Int('n_neurons', min_value=10, max_value=50)
+        learning_rate=hp.Float('learning_rate', min_value=1e-4, max_value=1e-1, sampling='log')
+        optimizer=hp.Choice('optimizer', values=['sgd', 'adam'])
+        hidden_activation_func=hp.Choice('hidden_activation_func', values=['relu', 'leaky_relu', 'elu', 'gelu', 'swish', 'mish'])
+        
+        
+        if optimizer=='sgd':
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate)
+        else:
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        
+        
+        hidden_layers_dict={} # store hidden layers with parameters in order (Python) in dictionary for ease of reference later
+        for h in range(1, n_hidden+1):
+            hidden_layers_dict[h]=tf.keras.layers.Dense(units=n_neurons, kernel_initializer='he_normal', activation=hidden_activation_func, name=f'hidden{h}')
+            
+        concat_layer=tf.keras.layers.Concatenate() # define concatenation layer, no execution yet
+        
+        # Define output layers
+        
+        output_layer=tf.keras.layers.Dense(units=2, activation='softmax', name='output_species') # need to vary n_neurons in output layer, 2 for binary classification and 6 for multiclass
+        
+        layers={} # store layers in order for actual construction of neural network
+        
+        # Pass executed layers in order
+        
+        layers['input']=tf.keras.layers.Input(shape=(3,))
+        
+        for k in hidden_layers_dict:
+            layers[f'hidden{k}']=hidden_layers_dict[k](layers[list(layers.keys())[-1]]) # layer output passed to hidden is the previous one before the 2 above just added
+            
+        layers['concatenated']=concat_layer([layers['input'], layers[list(layers.keys())[-1]]]) # last layer of the dictionary passed with the input layer for concatenation
+        layers['output_species']=output_layer1(layers['concatenated'])
+        
+        
+        neural_model_unsup=tf.keras.Model(inputs=[layers['input']], outputs=[layers['output_species']]) # specify model inputs and outputs
+        
+        neural_model_unsup.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy']) # compile model
+        
+        return neural_model_unsup
+        
+        
+    def fit(self, hp, neural_model, X, y, **kwargs):
+        """
+        Finetuning of second preprocessing (normalization) and fit parameters
+        """
+        batch_size=hp.Int('batch_size', min_value=10, max_value=100) # define possible values for batch size
+        
+        if hp.Boolean('normalize'): # allow normalization if yes
+            normalization_layer=tf.keras.layers.Normalization()
+            X=normalization_layer(X)
+        
+        return neural_model.fit(X, y, batch_size=batch_size, **kwargs)
---
+++ scripts/model_div_species.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+"""
+This script uses deep learning to model relationships between 2 input features (diversity measures) and 1 output feature (species)
+It performs a supervised binary classification
+Best model is saved on disk for future use
+Dependency: common_dl.py -> ClassificationSpecies
+"""
+
+import tensorflow as tf
+import pandas as pd
+import os
+
+from common_dl import ClassificationSpecies # import class for finetuning
+
+# 1. Read appropriate data as dataframe
+
+data = pd.read_csv('../data/dataset2_het_froh_species.csv')
+
+
+# 2. Plot features for quality control
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(figsize=(20, 10))
+data.hist(ax=ax, bins=50, color='black', alpha=0.7)
+plt.show()
+fig.savefig("../output/Quality_Control_Features"), dpi=500)
+
+
+# 3. Define training, validation and test sets
+
+from sklearn.model_selection import train_test_split # load utility for data splitting into different sets
+
+data_train_valid, data_test = train_test_split(data, test_size=0.1, random_state=2025)
+
+data_train, data_valid = train_test_split(data_train_valid, test_size=0.1, random_state=2025)
+
+
+# 4. Define input and output features for each set
+
+X_train, X_valid, X_test = data_train[['heterozygosity', 'FROH']], data_valid[['heterozygosity', 'FROH']], data_test[['heterozygosity', 'FROH']]
+
+y_train, y_valid, y_test = data_train['species'], data_valid['species'], data_test['species']
+
+
+# 5. Proceed to fine-tuning in search for best model
+
+import keras_tuner as kt
+
+tf.keras.utils.set_random_seed(2025) # set random seed for tensorflow utilities for reproducibility
+
+if os.path.exists('multi_species_modeling/best_model.keras'):
+    
+    print('The model has already been trained and saved on disk!')
+    
+    best_model=tf.keras.models.load_model('multi_species_modeling/best_model.keras')
+    
+elif os.path.exists('multi_species_modeling/best_checkpoint.keras'):
+    
+    print('The model has already been trained and saved on disk!')
+    
+    best_model=tf.keras.models.load_model('multi_species_modeling/best_checkpoint.keras')
+
+else:
+    
+    hyperband_tuner=kt.Hyperband(ClassificationSpecies(), objective='val_accuracy', seed=2025, max_epochs=10, factor=2, hyperband_iterations=2, overwrite=True, directory='multi_species_modeling', project_name='hyperband')
+        
+        checkpoint_cb = tf.keras.callbacks.ModelCheckpoint('multi_species_modeling/best_checkpoint.keras', save_best_only=True)
+
+        early_stopping_cb=tf.keras.callbacks.EarlyStopping(patience=2) # callback to prevent overfitting
+    
+        tensorboard_cb=tf.keras.callbacks.TensorBoard(Path(hyperband_tuner.project_dir)/'tensorflow'/strftime("run_%Y_%m_%d_%H_%M_%S")) # callback for tensorboard visualization
+    
+        hyperband_tuner.search(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid), callbacks=[checkpoint_cb, early_stopping_cb, tensorboard_cb])
+    
+        top3_models=hyperband_tuner.get_best_models(num_models=3)
+    
+        best_model=top3_models[0] # select the best model
+        
+        best_model.save('multi_species_modeling/best_model.keras') # save it
+
+
+# 6. Train the best model for longer
+
+#best_model.fit(X_train, y_train, validation_data=(X_valid, y_valid), epochs=10)
